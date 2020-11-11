9db3d89012eb7a4fbdcd788c8d0dc089cbfae9cd
taurus366 <47596673+taurus366@users.noreply.github.com>
2020-11-11 15:10:46 +0200

    mvc

--- src/main/java/course/springdata/restmvc/web/HelloController.java
+++ src/main/java/course/springdata/restmvc/web/HelloController.java
@@ -2,14 +2,15 @@ package course.springdata.restmvc.web;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello(){
-        return "HELLO From Spring MVC!";
+    public String sayHello(@RequestParam(value = "name",required = false,defaultValue = "not input") String name){
+        return String.format( "HELLO %s , From Spring MVC!",name);
     }
 
 }
