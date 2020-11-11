0eac4b14af09bf730911652799cd0f7ff3612fe7
taurus366 <47596673+taurus366@users.noreply.github.com>
2020-11-11 15:17:53 +0200

    mvc

--- src/main/java/course/springdata/restmvc/web/HelloController.java
+++ src/main/java/course/springdata/restmvc/web/HelloController.java
@@ -2,6 +2,7 @@ package course.springdata.restmvc.web;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "name",required = false,defaultValue = "not input") String name){
         return String.format( "HELLO %s , From Spring MVC!",name);
+    }
+
+    @GetMapping("/users/{userId}/post/{postId}")
+    public String getPost(@PathVariable Integer userId,@PathVariable Integer postId){
+        return String.format("User: %d --> Post: %d\n", userId,postId);
     }
 
 }
