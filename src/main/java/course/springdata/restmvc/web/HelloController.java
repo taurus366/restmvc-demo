b0a528649b1da72d167a7388e18b8a6b7f4c0ac7
taurus366 <47596673+taurus366@users.noreply.github.com>
2020-11-11 15:05:00 +0200

    mvc

--- src/main/java/course/springdata/restmvc/web/HelloController.java
+++ src/main/java/course/springdata/restmvc/web/HelloController.java
@@ -1,4 +1,15 @@
 package course.springdata.restmvc.web;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "HELLO From Spring MVC!";
+    }
+
 }
